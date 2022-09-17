25fdd068e902111773eac3f3747abc03bbcf870d
Lucas Dario Stocksmeier <lucas@stocksmeier.de>
2022-09-17 13:30:36 +0200

    fix: home service logic change

--- src/main/java/org/comppress/customnewsapi/repository/ArticleRepository.java
+++ src/main/java/org/comppress/customnewsapi/repository/ArticleRepository.java
@@ -36,11 +36,13 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
                 JOIN publisher p on p.id = rf.publisher_id
             WHERE rf.category_id = :categoryId
               AND p.id in :publisherIds
+              AND (:language is null or :language = '' or rf.lang LIKE :language)
               AND (:filterOutPaywallArticles = FALSE or a.paywall_article = FALSE)
               AND a.published_at is not null Order by a.published_at DESC Limit 1
             """, nativeQuery = true)
     CustomArticle retrieveLatestArticleOfCategory(
             @Param("categoryId") Long categoryId,
+            @Param("language") String language,
             @Param("filterOutPaywallArticles") Boolean filterOutPaywallArticles,
             @Param("publisherIds") List<Long> publisherIds
     );
@@ -64,17 +66,17 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
               AND a.published_at BETWEEN IFNULL(:fromDate, '1900-01-01 00:00:00') AND IFNULL(:toDate,now())
             """,
             countQuery = """
-            Select count(*) from article a
-                     JOIN rss_feed rf on rf.id = a.rss_feed_id
-                     JOIN category c on c.id = rf.category_id
-                     JOIN publisher p on p.id = rf.publisher_id
-            WHERE (:category is null or :category = '' or c.name LIKE %:category%)
-              AND (:publisherName is null or :publisherName = '' or p.name LIKE %:publisherName%)
-              AND (:title is null or :title = '' or a.title LIKE %:title%)
-              AND (:language is null or :language = '' or rf.lang LIKE :language)
-              AND (:filterOutPaywallArticles = FALSE or a.paywall_article = FALSE)
-              AND a.published_at BETWEEN IFNULL(:fromDate, '1900-01-01 00:00:00') AND IFNULL(:toDate,now())
-            """, nativeQuery = true)
+                    Select count(*) from article a
+                             JOIN rss_feed rf on rf.id = a.rss_feed_id
+                             JOIN category c on c.id = rf.category_id
+                             JOIN publisher p on p.id = rf.publisher_id
+                    WHERE (:category is null or :category = '' or c.name LIKE %:category%)
+                      AND (:publisherName is null or :publisherName = '' or p.name LIKE %:publisherName%)
+                      AND (:title is null or :title = '' or a.title LIKE %:title%)
+                      AND (:language is null or :language = '' or rf.lang LIKE :language)
+                      AND (:filterOutPaywallArticles = FALSE or a.paywall_article = FALSE)
+                      AND a.published_at BETWEEN IFNULL(:fromDate, '1900-01-01 00:00:00') AND IFNULL(:toDate,now())
+                    """, nativeQuery = true)
     Page<CustomArticle> retrieveByCategoryOrPublisherNameToCustomArticle(
             @Param("category") String category,
             @Param("publisherName") String publisherName,
@@ -124,34 +126,34 @@ public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
 
 
     @Query(value = """
-select a.id, a.author, a.title, a.description, a.url, a.url_to_image as urlToImage, a.published_at as publishedAt,
-       a.count_ratings as countRatings, a.count_comment as countComment, a.paywall_article as paywallArticle, a.scale_image as scaleImage,
-       rf.category_id as categoryId, rf.publisher_id as publisherId,
-       c.name as categoryName,
-       p.name as publisherName,
-t.average_rating_criteria_1 as averageRatingCriteria1, t.average_rating_criteria_2 as averageRatingCriteria2, t.average_rating_criteria_3 as averageRatingCriteria3,
-       sum(t.average_rating_criteria_1 + t.average_rating_criteria_2 + t.average_rating_criteria_3)/
-       (CASE WHEN t.average_rating_criteria_1 IS NULL THEN 0 ELSE 1 END +
-        CASE WHEN t.average_rating_criteria_2 IS NULL THEN 0 ELSE 1 END +
-        CASE WHEN t.average_rating_criteria_3 IS NULL THEN 0 ELSE 1 END)
-            AS totalAverageRating from (
-                SELECT distinct r.article_id,
-                              (select avg(r1.rating) from rating r1 where r1.article_id = r.article_id AND r1.criteria_id=1) as average_rating_criteria_1,
-                              (select avg(r1.rating) from rating r1 where r1.article_id = r.article_id AND r1.criteria_id=2) as average_rating_criteria_2,
-                              (select avg(r1.rating) from rating r1 where r1.article_id = r.article_id AND r1.criteria_id=3) as average_rating_criteria_3
-                FROM rating r group by r.article_id
-            )as t
-                INNER JOIN article a ON a.id= t.article_id
-                INNER JOIN rss_feed rf ON rf.id = a.rss_feed_id
-                INNER JOIN category c ON c.id = rf.category_id
-                INNER JOIN publisher p ON p.id = rf.publisher_id
-           WHERE ( rf.lang = :lang )
-             AND c.id = :categoryId
-             AND p.id in :publisherIds
-             AND (:filterOutPaywallArticles = FALSE or a.paywall_article = FALSE)
-             AND a.published_at BETWEEN IFNULL(:fromDate, '1900-01-01 00:00:00') AND IFNULL(:toDate,now())
-            group by t.article_id order by totalAverageRating DESC LIMIT 1
-            """, nativeQuery = true)
+            select a.id, a.author, a.title, a.description, a.url, a.url_to_image as urlToImage, a.published_at as publishedAt,
+                   a.count_ratings as countRatings, a.count_comment as countComment, a.paywall_article as paywallArticle, a.scale_image as scaleImage,
+                   rf.category_id as categoryId, rf.publisher_id as publisherId,
+                   c.name as categoryName,
+                   p.name as publisherName,
+            t.average_rating_criteria_1 as averageRatingCriteria1, t.average_rating_criteria_2 as averageRatingCriteria2, t.average_rating_criteria_3 as averageRatingCriteria3,
+                   sum(t.average_rating_criteria_1 + t.average_rating_criteria_2 + t.average_rating_criteria_3)/
+                   (CASE WHEN t.average_rating_criteria_1 IS NULL THEN 0 ELSE 1 END +
+                    CASE WHEN t.average_rating_criteria_2 IS NULL THEN 0 ELSE 1 END +
+                    CASE WHEN t.average_rating_criteria_3 IS NULL THEN 0 ELSE 1 END)
+                        AS totalAverageRating from (
+                            SELECT distinct r.article_id,
+                                          (select avg(r1.rating) from rating r1 where r1.article_id = r.article_id AND r1.criteria_id=1) as average_rating_criteria_1,
+                                          (select avg(r1.rating) from rating r1 where r1.article_id = r.article_id AND r1.criteria_id=2) as average_rating_criteria_2,
+                                          (select avg(r1.rating) from rating r1 where r1.article_id = r.article_id AND r1.criteria_id=3) as average_rating_criteria_3
+                            FROM rating r group by r.article_id
+                        )as t
+                            INNER JOIN article a ON a.id= t.article_id
+                            INNER JOIN rss_feed rf ON rf.id = a.rss_feed_id
+                            INNER JOIN category c ON c.id = rf.category_id
+                            INNER JOIN publisher p ON p.id = rf.publisher_id
+                       WHERE ( rf.lang = :lang )
+                         AND c.id = :categoryId
+                         AND p.id in :publisherIds
+                         AND (:filterOutPaywallArticles = FALSE or a.paywall_article = FALSE)
+                         AND a.published_at BETWEEN IFNULL(:fromDate, '1900-01-01 00:00:00') AND IFNULL(:toDate,now())
+                        group by t.article_id order by totalAverageRating DESC LIMIT 1
+                        """, nativeQuery = true)
     CustomRatedArticle retrieveOneRatedArticleByCategoryIdsAndPublisherIdsAndLanguageAndLimit(
             @Param("categoryId") Long categoryId,
             @Param("publisherIds") List<Long> publisherIds,
--- src/main/java/org/comppress/customnewsapi/service/home/HomeService.java
+++ src/main/java/org/comppress/customnewsapi/service/home/HomeService.java
@@ -3,20 +3,20 @@ package org.comppress.customnewsapi.service.home;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.comppress.customnewsapi.dto.CustomCategoryDto;
+import org.comppress.customnewsapi.dto.GenericPage;
 import org.comppress.customnewsapi.dto.article.CustomArticleDto;
 import org.comppress.customnewsapi.dto.article.CustomRatedArticleDto;
-import org.comppress.customnewsapi.dto.GenericPage;
 import org.comppress.customnewsapi.entity.AbstractEntity;
 import org.comppress.customnewsapi.entity.CategoryEntity;
 import org.comppress.customnewsapi.entity.PublisherEntity;
 import org.comppress.customnewsapi.entity.UserEntity;
 import org.comppress.customnewsapi.entity.article.CustomArticle;
+import org.comppress.customnewsapi.entity.article.CustomRatedArticle;
 import org.comppress.customnewsapi.mapper.MapstructMapper;
 import org.comppress.customnewsapi.repository.ArticleRepository;
 import org.comppress.customnewsapi.repository.CategoryRepository;
 import org.comppress.customnewsapi.repository.PublisherRepository;
 import org.comppress.customnewsapi.repository.UserRepository;
-import org.comppress.customnewsapi.entity.article.CustomRatedArticle;
 import org.comppress.customnewsapi.service.BaseSpecification;
 import org.comppress.customnewsapi.service.twitter.TwitterService;
 import org.comppress.customnewsapi.utils.DateUtils;
@@ -32,6 +32,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -48,73 +49,125 @@ public class HomeService implements BaseSpecification {
     private final TwitterService twitterService;
     private final MapstructMapper mapstructMapper;
 
-    public List<Long> getPublisher(List<Long> publisherIds, String lang){
+    public List<Long> getPublisher(List<Long> publisherIds, String lang) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserEntity userEntity = userRepository.findByUsernameAndDeletedFalse(authentication.getName());
 
-        if(userEntity != null){
-            if(userEntity.getListPublisherIds() != null){
+        if (userEntity != null) {
+            if (userEntity.getListPublisherIds() != null) {
                 publisherIds = Stream.of(userEntity.getListPublisherIds().split(",")).map(Long::parseLong).collect(Collectors.toList());
             }
         }
-        if (publisherIds == null || publisherIds.isEmpty() || doesNotContainAnyPublishersFromLang(publisherIds,lang)) {
-            publisherIds = publisherRepository.findByLang(lang).stream().map(publisher -> publisher.getId()).collect(Collectors.toList());
+        List<Long> publisherIdsByLang = publisherRepository.findByLang(lang).stream().map(AbstractEntity::getId).collect(Collectors.toList());
+        if (publisherIds == null || publisherIds.isEmpty()) {
+            publisherIds = publisherIdsByLang;
+        } else {
+            List<Long> newPublisherIds = new ArrayList<>();
+            List<Long> finalPublisherIdsByLang = publisherIdsByLang;
+            publisherIds.stream().forEach(publisherId -> {
+                if (finalPublisherIdsByLang.contains(publisherId)) {
+                    newPublisherIds.add(publisherId);
+                }
+            });
+            publisherIdsByLang = newPublisherIds;
         }
         return publisherIds;
     }
 
-    public List<Long> getCategory(List<Long> categoryIds, String lang){
+    public List<Long> getCategory(List<Long> categoryIds, String lang) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserEntity userEntity = userRepository.findByUsernameAndDeletedFalse(authentication.getName());
 
-        if(userEntity != null){
-            if(userEntity.getListCategoryIds() != null){
+        if (userEntity != null) {
+            if (userEntity.getListCategoryIds() != null) {
                 categoryIds = Stream.of(userEntity.getListCategoryIds().split(",")).map(Long::parseLong).collect(Collectors.toList());
             }
         }
 
-        if (categoryIds == null || categoryIds.isEmpty() || doesNotContainAnyCategoriesFromLang(categoryIds,lang)) {
-            categoryIds = categoryRepository.findByLang(lang).stream().map(category -> category.getId()).collect(Collectors.toList());
+        List<Long> categoryIdsByLang = categoryRepository.findByLang(lang).stream().map(category -> category.getId()).collect(Collectors.toList());
+        if (categoryIds == null || categoryIds.isEmpty()) {
+            categoryIds = categoryIdsByLang;
+        } else {
+            List<Long> newCategoryIds = new ArrayList<>();
+            categoryIds.stream().forEach(categoryId -> {
+                if (categoryIdsByLang.contains(categoryId)) {
+                    newCategoryIds.add(categoryId);
+                }
+            });
+            categoryIds = newCategoryIds;
         }
+
         return categoryIds;
     }
 
+    /**
+     * Logic for the Landing Page
+     * - Try to display the best rated Article for every Category of the last 24 hours
+     * - You can pass a categoryIds list, only the categories specified should be returned
+     * - If you pass no categories all are selected
+     * - If there is no rated Article in the last 24 hours we display the latest article, this way we make sure to always return an article
+     *
+     * @param page
+     * @param size
+     * @param lang
+     * @param categoryIds
+     * @param publisherIds
+     * @param fromDate
+     * @param toDate
+     * @param filterOutPaywallArticles
+     * @return
+     */
     public ResponseEntity<GenericPage> getHome(int page, int size, String lang, List<Long> categoryIds,
                                                List<Long> publisherIds, String fromDate, String toDate, Boolean filterOutPaywallArticles) {
-        // if Date not set, retrieve results for last 24 hours
-        if(fromDate == null && toDate == null){
+        // if fromDate and toDate null retrieve the last 24 hours
+        fromDate = getDate(fromDate, toDate);
+
+        // retrieve all publisherIds of lang if null or empty, also retrieve if user has configured publisher preferences
+        final List<Long> finalPubIds = getPublisher(publisherIds, lang);
+
+        // retrieve all categoryIds of lang if null or empty, also retrieve if user has configured category preferences
+        // makes sure only CategoryIds of language are returned
+        categoryIds = getCategory(categoryIds, lang);
+
+        String finalFromDate = fromDate;
+        List<CategoryEntity> categoryEntityList = categoryRepository.findByCategoryIds(categoryIds);
+
+        // For every category in categoryEntityList retrieve news
+        List<CustomCategoryDto> customCategoryDtos = categoryEntityList.stream().map(
+                category -> getArticlesForCategory(category, lang,
+                        finalPubIds, DateUtils.stringToLocalDateTime(finalFromDate), DateUtils.stringToLocalDateTime(toDate), filterOutPaywallArticles)
+        ).collect(Collectors.toList());
+        return PageHolderUtils.getResponseEntityGenericPage(page, size, customCategoryDtos);
+    }
+
+    private String getDate(String fromDate, String toDate) {
+        if (fromDate == null && toDate == null) {
             Instant instant = Instant.now().minus(24, ChronoUnit.HOURS);
             Timestamp timestamp = Timestamp.from(instant);
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             fromDate = timestamp.toLocalDateTime().format(formatter);
             log.info(fromDate);
         }
-
-        final List<Long> finalPubIds = getPublisher(publisherIds, lang);
-        categoryIds = getCategory(categoryIds,lang);
-
-        String finalFromDate = fromDate;
-        List<CustomCategoryDto> customCategoryDtos = categoryRepository.
-                findByCategoryIds(categoryIds).stream().map(s -> setArticles(s, lang,
-                finalPubIds, DateUtils.stringToLocalDateTime(finalFromDate), DateUtils.stringToLocalDateTime(toDate), filterOutPaywallArticles)).collect(Collectors.toList());
-        return PageHolderUtils.getResponseEntityGenericPage(page,size,customCategoryDtos);
+        return fromDate;
     }
 
-    private CustomCategoryDto setArticles(CategoryEntity category, String lang,
-                                          List<Long> publisherIds, LocalDateTime fromDate, LocalDateTime toDate, Boolean filterOutPaywallArticles) {
+    private CustomCategoryDto getArticlesForCategory(CategoryEntity category, String lang,
+                                                     List<Long> publisherIds, LocalDateTime fromDate, LocalDateTime toDate, Boolean filterOutPaywallArticles) {
         // TODO Limit 1, Publishers included, Rated
+        /*
         if (publisherIds == null || publisherIds.isEmpty()) {
             publisherIds = publisherRepository.findByLang(lang).stream().map(AbstractEntity::getId).collect(Collectors.toList());
         }
+         */
         Long categoryId = category.getId();
-        CustomRatedArticle article = articleRepository.retrieveOneRatedArticleByCategoryIdsAndPublisherIdsAndLanguageAndLimit(categoryId,publisherIds,lang,fromDate,toDate,filterOutPaywallArticles);
+        CustomRatedArticle article = articleRepository.retrieveOneRatedArticleByCategoryIdsAndPublisherIdsAndLanguageAndLimit(categoryId, publisherIds, lang, fromDate, toDate, filterOutPaywallArticles);
         CustomCategoryDto customCategoryDto = new CustomCategoryDto();
-        if(article != null){
+        if (article != null) {
             CustomRatedArticleDto customRatedArticleDto = mapstructMapper.customRatedArticleToCustomRatedArticleDto(article);
             customCategoryDto.setArticle(customRatedArticleDto);
             twitterService.setReplyCount(customRatedArticleDto);
         } else {
-            CustomArticle customArticle = articleRepository.retrieveLatestArticleOfCategory(categoryId, filterOutPaywallArticles, publisherIds);
+            CustomArticle customArticle = articleRepository.retrieveLatestArticleOfCategory(categoryId, lang, filterOutPaywallArticles, publisherIds);
             CustomArticleDto customArticleDto = mapstructMapper.customArticleToCustomArticleDto(customArticle);
             customCategoryDto.setArticle(customArticleDto);
         }
@@ -124,8 +177,8 @@ public class HomeService implements BaseSpecification {
     }
 
     private boolean doesNotContainAnyCategoriesFromLang(List<Long> listCategoryIds, String lang) {
-        for(CategoryEntity category:categoryRepository.findByLang(lang)){
-            if(listCategoryIds.contains(category.getId())) return false;
+        for (CategoryEntity category : categoryRepository.findByLang(lang)) {
+            if (listCategoryIds.contains(category.getId())) return false;
         }
         return true;
     }
